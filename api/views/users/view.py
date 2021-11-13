9992c2fb0204c700e190e01941fac4cb2ec01608
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-12 22:39:53 -0300

    Refatoração da view para melhor organização

--- api/views/users/view.py
+++ api/views/users/view.py
@@ -1,6 +1,5 @@
 from flask import request
 from flask.views import MethodView
-
 from api.models.user import User
 from api.utils.auth import encode_password
 
@@ -10,17 +9,11 @@ class UserView(MethodView):
   def __init__(self) -> None:
     super().__init__()
 
-  def get(self, request=request):
-    pass
-
   def post(self, request=request):
-    username = request.json.get('name')
-    password = request.json.get('password')
-    email = request.json.get('email')
-    user = User(
-      name=username,
-      password=encode_password(email, password),
-      email=email
-    )
+    user = User(**{
+      'name': request.json.get('name'),
+      'email': request.json.get('email'),
+      'password': encode_password(request.json.get('email'), request.json.get('password')),
+    })
     user.save()
     return user.to_dict()
