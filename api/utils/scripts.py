f692b53fd7fe4815361e83997a88636e0b801b65
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-15 20:10:05 -0300

    Adição de scripts para criação de banco e tabela

---
+++ api/utils/scripts.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+engine = create_engine('mysql+pymysql://root:123456@localhost/db?charset=utf8mb4', echo=True)  
+
+def create_databases():
+  if not database_exists(engine.url):
+    create_database(engine.url)
+
+def create_tables():
+  meta = MetaData()
+  users = Table(
+    'users', meta, 
+    Column('id', Integer, primary_key = True), 
+    Column('name', String(255)), 
+    Column('email', String(255)),
+    Column('password', String(255))
+  )
+  meta.create_all(engine)
--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import os
 from api import application
+from dotenv import load_dotenv
+from api.utils.scripts import create_databases, create_tables
+
+load_dotenv(os.path.join('./.env')) 
+
+create_databases()
+create_tables()
 
 app = application()
 
