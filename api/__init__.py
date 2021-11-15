462d26380e99e55c6b611e10b31d84b37af242c2
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-15 20:11:19 -0300

    Ajuste na config do db

--- api/__init__.py
+++ api/__init__.py
@@ -1,22 +1,26 @@
 from flask import Flask
 from api.urls import routes
 from flask_restful import Api
-from api.models import db
+from api.models import db 
 from flask_migrate import Migrate
+from api.config import DATABASE_URL
 
 def sql_alchemy(app):
   return db.init_app(app)
 
-def flask_restful(flask=Flask):
+def flask_instance(flask=Flask):
   app = flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/db?charset=utf8mb4'
+  app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
   return app
 
-def application():
-  app = flask_restful()
-  db = sql_alchemy(app)
-  Migrate(app, db)
+def flask_restful(app):
   api = Api(app)
   api.prefix = '/api'
+  return api
+
+def application():
+  app = flask_instance()
+  api = flask_restful(app)
+  sql_alchemy(app)
   routes(api)
   return app
