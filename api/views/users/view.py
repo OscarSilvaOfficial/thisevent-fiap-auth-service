9b92bf1a00f51d87e052de6598cefe8964dc6672
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-15 20:12:05 -0300

    Criptografando senha

--- api/views/users/view.py
+++ api/views/users/view.py
@@ -2,6 +2,7 @@ from flask import request
 from api.models.user import User
 from flask.views import MethodView
 from api.utils.request import parse_request
+from api.utils.auth import encode_password
 from api.views.users.parser import parser
 
 
@@ -12,6 +13,10 @@ class UserView(MethodView):
 
   @parse_request(request, parser)
   def post(self, request):
+    request['password'] = encode_password(
+      password=request['password'],
+      email=request['email']
+    ) 
     user = User(**request)
     user.save()
     return user.to_dict() 
