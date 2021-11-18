3de65bf8f3357e78ba54fdecba6ede79c98b8243
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-17 23:23:19 -0300

    Ajuste na porta que roda

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import os
 from api import application
-from dotenv import load_dotenv
 from api.utils.scripts import create_databases, create_tables
-
-load_dotenv(os.path.join('./.env')) 
 
 create_databases()
 create_tables()
@@ -11,4 +7,4 @@ create_tables()
 app = application()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(port=5001, debug=False)
