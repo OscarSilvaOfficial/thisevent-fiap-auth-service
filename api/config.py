c44db4ac5e843ec1c2c7f51646f8cd32135ae537
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-08 22:15:22 -0300

    update

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
 import os
 
 SECRET=os.getenv('SECRET', 'ashdfashdfuioahdeusifhjiaudhgvq390713984ryfuhif')
-DATABASE_URL=os.getenv('DATABASE_URL', 'mysql+pymysql://root:123456@localhost:3306/admin?charset=utf8mb4')
+DATABASE_URL=os.getenv('DATABASE_URL', 'mysql+pymysql://admin:admin@localhost:3306/admin?charset=utf8mb4')
